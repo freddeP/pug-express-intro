b46b1d483f054ca0a3be6a7ad157087098c15b3a
Fredric Persson <freper7912@skynet.halmstad.se>
2019-04-30 15:39:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+// För att kunna läsa in css och front-end-js från public
+app.use(express.static(__dirname+"/public"));
+
+// Definierar vilken katalog som skall användas till vyer
+app.set('views', './views');
+
+// Definiera vilken vy-motor som skall användas.
+// express kör eventuella pug-kommandon i bakgrunden.
+app.set('view engine', 'pug')
+const names = ["Lenny","Jane", "Benny", "Sören", "Jens-Peter"];
+app.get('/', function (req, res) {
+res.render('index', { title: 'Express PUG', names })
+})
+
+
+app.listen(2400,function(){console.log("port"+2400)})
